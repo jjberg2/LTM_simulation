4005a662ce9a71ed9fa914eedc5109cb88bef4d2
Jeremy J Berg <jjberg@uchicago.edu>
2023-04-24 08:36:58 -0500

    added large effect insensitivity param and snakemake files

---
+++ largeEffectInsensitivity_snakemake.py
@@ -0,0 +1,56 @@
+import numpy as np 
+import pandas as pd
+
+params_table = pd.read_csv("largeEffectInsensitivityInsensitivityParamTable.txt", delim_whitespace=True)
+
+
+## global parameter ( doesn't change) 
+mu=1e-6
+cyc = 100
+sampleInt = 50
+
+print(params_table)
+
+## simulation variable 
+rep = list(np.arange(0,1))
+rhos = np.array(params_table["rho"])
+liaSizes = np.array((params_table["target.size"]).astype(int))
+cost = np.round((params_table["cost"]),3)
+rhos = np.round(np.array(params_table["rho"]),3)
+N = np.array(params_table["Ne"].astype(int))
+
+print(cost)
+
+rule all:
+  input: 
+    expand(expand("CostInsensitivity/PopSize{N}_LiaSize{liaSizes}_rho{rhos}_cost{cost}_rep{{rep}}.prev",zip, N=N, liaSizes=liaSizes, rhos=rhos, cost=cost), rep=rep),
+    #expand("PopSize{N}_LiaSize{liaSizes}_rho{rhos}_all.h2", zip, N=N, liaSizes=liaSizes, rhos=rhos)
+
+rule slim_simulate_withsegregating:
+  input:
+    slim_script="LTM_prev_nucleotide.slim"
+  params:
+    mu=mu,
+    cyc=cyc, 
+    sampleInt = sampleInt, 
+    time="50:00:00",
+##    partition="jjberg",
+    mem="5Gb"
+  output:
+    "CostInsensitivity/PopSize{N}_LiaSize{liaSizes}_rho{rhos}_cost{cost}_rep{rep}.prev"
+  shell:
+    """thr=`awk 'BEGIN {{print 1e5*2*{wildcards.rhos}}}'`;
+    env=`Rscript --vanilla scripts/getEnvSD.R 1e-6 ${{thr}} 0.9 5000 1e5 0.5 | awk '{{print $2}}'`;
+    set +u; slim  -d mu={params.mu} -d rho_input={wildcards.rhos} -d p={wildcards.N} -d liaSize={wildcards.liaSizes} -d f={wildcards.cost}  -d e=${{env}} -d cyc={params.cyc} -d sampleInt={params.sampleInt} -d rep={wildcards.rep} {input.slim_script} > PopSize{wildcards.N}_LiaSize{wildcards.liaSizes}_rho{wildcards.rhos}_rep{wildcards.rep}.temp; set -u;""" 
+    #set +u; rm PopSize{wildcards.N}_LiaSize{wildcards.liaSizes}_rho{wildcards.rhos}_rep{wildcards.rep}.temp; set -u"""
+
+rule result_combined: 
+   input: 
+     h2=expand("CostInsensitivity/PopSize{{N}}_LiaSize{{liaSizes}}_rho{{rhos}}_cost{{cost}}_rep{rep}.h2", rep=rep),
+     prev=expand("CostInsensitivity/PopSize{{N}}_LiaSize{{liaSizes}}_rho{{rhos}}_cost{{cost}}_rep{rep}.prev", rep=rep)
+   output:
+     h2="CostInsensitivity/PopSize{N}_LiaSize{liaSizes}_rho{rhos}_cost{cost}_all.h2",
+     prev="CostInsensitivity/PopSize{N}_LiaSize{liaSizes}_rho{rhos}_cost{cost}_all.prev"
+   shell: 
+     """cat {input.h2} >> {output.h2}; cat {input.prev} >> {output.prev}""" 
+ 
---
+++ scripts/makeLargeEffectInsensitivityParamFile.R
@@ -0,0 +1,43 @@
+library('viridis')
+source('scripts/solveSingleEffect.R')
+
+my.thr <- c(1,2,5,10,15,20,50)
+my.Ne <- 5000
+L <- 10000
+h2=0.5
+mu <- 1e-6
+my.theta <- 4*my.Ne*mu
+
+
+sim.costs <- c(0.05,0.25,0.5,0.75,0.95)
+sim.out <- list()
+for(j in seq_along(my.thr)){
+    sim.out[[j]] <- list()
+    for(i in seq_along(sim.costs)){
+        sim.out[[j]][[i]] <- SolvePoissonGivenThr(myTheta=my.theta,thr=my.thr[j],fitCost=sim.costs[i],Ne=my.Ne,L=L,h2=h2,verbose.output=TRUE)
+    }
+    print(j)
+}
+
+my.table <- expand.grid(sim.costs,my.thr,L,my.theta,h2,my.Ne)
+colnames(my.table) <- c('cost','thr','target.size','theta','h2','Ne')
+my.rho <- my.table[,'thr']/(2*my.table[,'target.size'])
+my.b <- 1-2*my.rho
+my.gamma <- log((1+my.b)/(1-my.b))
+env.sd <- numeric()
+pois.prev <- numeric()
+for(j in seq_along(sim.out)){
+    for(i in seq_along(sim.out[[j]])){
+        my.out <- sim.out[[j]][[i]]
+        if(length(my.out)==1){
+            env.sd <- append(env.sd,NA)
+            pois.prev <- append(pois.prev,NA)
+        } else{
+            env.sd <- append(env.sd,my.out$env.sd)
+            pois.prev <- append(pois.prev,my.out$prev)
+        }
+    }
+}
+new.table <- cbind(my.table,'rho'=my.rho,'b'=my.b,'gamma'=my.gamma,'env.sd'=env.sd,'pois.prev'=pois.prev)
+
+write.table(new.table,file='largeEffectInsensitivityParamFile.txt')
