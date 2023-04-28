1121de3d8f75b705f82683ae9f9372464d4b7677
Jeremy J Berg <jjberg@uchicago.edu>
2023-04-28 14:27:52 -0500

    adding output of number of seg sites for two effect model

--- twoEffectInsensitivity_snakemake.py
+++ twoEffectInsensitivity_snakemake.py
@@ -11,7 +11,7 @@ params_table = pd.read_csv("twoEffectInsensitivity/twoEffectInsensitivityParamsT
 cyc = 200
 sampleInt = 50
 reps = 3
-toyRun = 0
+toyRun = 1
 
 
 ## simulation variable 
@@ -43,7 +43,9 @@ rule slim_simulate_withsegregating:
     prev="{prefix}/PopSize{N}_aL{alphaLarge}_rhot{rhot}_envSD{envSD}_cost{cost}_rep{rep}.prev",
     h2l="{prefix}/PopSize{N}_aL{alphaLarge}_rhot{rhot}_envSD{envSD}_cost{cost}_rep{rep}.h2l",
     h2s="{prefix}/PopSize{N}_aL{alphaLarge}_rhot{rhot}_envSD{envSD}_cost{cost}_rep{rep}.h2s",
-    genVar="{prefix}/PopSize{N}_aL{alphaLarge}_rhot{rhot}_envSD{envSD}_cost{cost}_rep{rep}.genVar"
+    genVar="{prefix}/PopSize{N}_aL{alphaLarge}_rhot{rhot}_envSD{envSD}_cost{cost}_rep{rep}.genVar",
+    nSegSmall="{prefix}/PopSize{N}_aL{alphaLarge}_rhot{rhot}_envSD{envSD}_cost{cost}_rep{rep}.nSegSmall",
+    nSegLarge="{prefix}/PopSize{N}_aL{alphaLarge}_rhot{rhot}_envSD{envSD}_cost{cost}_rep{rep}.nSegLarge"
   params:
     mu = lambda wildcards: find_index(wildcards, col="u"),
     fitCost= lambda wildcards: find_index(wildcards, col="C"),
@@ -58,7 +60,7 @@ rule slim_simulate_withsegregating:
     partition="broadwl",
     mem="2Gb"
   shell:
-    """set +u; slim  -d mu={params.mu} -d rhot={wildcards.rhot} -d rhos={params.rhos} -d p={wildcards.N}  -d f={params.fitCost}  -d e={wildcards.envSD} -d cyc={params.cyc} -d sampleInt={params.sampleInt} -d rep={wildcards.rep} -d aS={params.alphaSmall} -d aL={wildcards.alphaLarge} -d liaSmall={params.liaSmall} -d liaLarge={params.liaLarge} -d "meanOut='{output.mean}'" -d "h2Out='{output.h2}'" -d "prevOut='{output.prev}'" -d "h2lOut='{output.h2l}'" -d "h2sOut='{output.h2s}'" -d toyRun={params.toyRun} {input.slim_script} > {wildcards.prefix}/PopSize{wildcards.N}_aL{wildcards.alphaLarge}_rhot{wildcards.rhot}_envSD{wildcards.envSD}_rep{wildcards.rep}.temp; set -u; 
+    """set +u; slim -d mu={params.mu} -d rhot={wildcards.rhot} -d rhos={params.rhos} -d p={wildcards.N}  -d f={params.fitCost}  -d e={wildcards.envSD} -d cyc={params.cyc} -d sampleInt={params.sampleInt} -d rep={wildcards.rep} -d aS={params.alphaSmall} -d aL={wildcards.alphaLarge} -d liaSmall={params.liaSmall} -d liaLarge={params.liaLarge} -d "meanOut='{output.mean}'" -d "h2Out='{output.h2}'" -d "prevOut='{output.prev}'" -d "h2lOut='{output.h2l}'" -d "h2sOut='{output.h2s}'" -d "nSegSmallOut='{output.nSegSmall}'"  -d "nSegLargeOut='{output.nSegLarge}'"-d toyRun={params.toyRun} {input.slim_script} > {wildcards.prefix}/PopSize{wildcards.N}_aL{wildcards.alphaLarge}_rhot{wildcards.rhot}_envSD{wildcards.envSD}_rep{wildcards.rep}.temp; set -u; 
     set +u; rm {wildcards.prefix}/PopSize{wildcards.N}_aL{wildcards.alphaLarge}_rhot{wildcards.rhot}_envSD{wildcards.envSD}_rep{wildcards.rep}.temp; set -u; """
 
 
