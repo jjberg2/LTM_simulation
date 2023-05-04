902953d2e2a5facb399f6ea33eb6d8bdb4b5cbae
Jeremy J Berg <jjberg@uchicago.edu>
2023-05-04 12:04:35 -0500

    gonna try a bigger run of the large effects

--- scripts/makeLargeEffectInsensitivityParamFile.R
+++ scripts/makeLargeEffectInsensitivityParamFile.R
@@ -12,7 +12,7 @@ my.theta <- 4*my.Ne*mu
 
 
 ## sim.costs <- seq(0.01,1,by=0.01)
-sim.costs <- c(0.25,0.5,0.75,0.95)
+sim.costs <- c(0.05,0.25,0.5,0.75,0.95)
 sim.out <- list()
 for(j in seq_along(my.thr)){
     sim.out[[j]] <- list()
