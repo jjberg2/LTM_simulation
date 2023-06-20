2ce9aefab8ef3c863244c12359ed93aeb265fa2f
Jeremy J Berg <jjberg@uchicago.edu>
2023-06-20 14:21:20 -0500

    blah

--- scripts/plotSingleEffectDerProbs.R
+++ scripts/plotSingleEffectDerProbs.R
@@ -14,14 +14,88 @@ for(i in seq_along(gammas)){
 
 
 
-pdf('../figures/singleEffectDerProbs.pdf',width=8,height=8)
-plot(NA,xlim=c(0,1),ylim=c(0,1),bty='n',type='l')
+pdf('figures/singleEffectDerProbs1.pdf',width=8,height=8)
+draw.n <- length(split.dps)
+plot(
+    NA,
+    xlim=c(0,1),
+    ylim=c(0,1),
+    bty='n',
+    type='l',
+    xlab='Frequency of Risk Increasing Allele',
+    ylab='P(risk allele is derived | frequency of risk allele)'
+)
 cols <- viridis(length(split.dps))
-for(j in 1:5){
+for(j in 1:draw.n){
     f <- as.numeric(row.names(split.dps[[j]]))
-    points(f,rowMeans(split.dps[[j]]),col=cols[j],pch=20,cex=0.3)
+    unambiguous <- f!=0.5
+    points(f[unambiguous],rowMeans(split.dps[[j]])[unambiguous],col=cols[j],pch=20,cex=0.3)
     g <- gammas[j]
     exp.dps <- (exp(g)-exp(g*f))/(exp(g)-1)
     lines(f,exp.dps,col=cols[j],lwd=2,lty=2)
 }
+legend(
+    'bottomleft',
+    legend=small.gamma[1:draw.n],
+    pch=20,
+    col=cols
+)
+text(
+    x=0.2,
+    y=0.4,
+    labels=expression((e^y-e^(y*x))/(e^y-e))
+)
 dev.off()
+
+
+
+
+
+
+pdf('figures/singleEffectDerProbs2.pdf',width=8,height=8)
+draw.n <- 4
+plot(
+    NA,
+    xlim=c(0,1),
+    ylim=c(0,1),
+    bty='n',
+    type='l',
+    xlab='Frequency of Risk Increasing Allele',
+    ylab='P(risk allele is derived | frequency of risk allele)'
+)
+cols <- viridis(length(split.dps))
+for(j in 1:draw.n){
+    f <- as.numeric(row.names(split.dps[[j]]))
+    unambiguous <- f!=0.5
+    points(f[unambiguous],rowMeans(split.dps[[j]])[unambiguous],col=cols[j],pch=20,cex=0.3)
+    g <- gammas[j]
+    exp.dps <- (exp(g)-exp(g*f))/(exp(g)-1)
+    lines(f,exp.dps,col=cols[j],lwd=2,lty=2)
+}
+legend(
+    'bottomleft',
+    legend=small.gamma[1:draw.n],
+    pch=20,
+    col=cols
+)
+text(
+    x=0.2,
+    y=0.4,
+    labels=expression((e^y-e^(y*x))/(e^y-e))
+)
+dev.off()
+
+
+
+
+
+
+
+
+
+tf <- F
+if(tf){
+    draw.n <- length(split.dps)
+} else {
+    draw.n <- 4
+}
