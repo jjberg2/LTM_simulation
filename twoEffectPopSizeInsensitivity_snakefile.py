350f46584d7e2771e4f5d007ebd7eb212d0325a2
Jeremy J Berg <jjberg@uchicago.edu>
2023-09-07 11:53:05 -0500

    added some reps to the popsize snakemake

--- twoEffectPopSizeInsensitivity_snakefile.py
+++ twoEffectPopSizeInsensitivity_snakefile.py
@@ -20,7 +20,7 @@ params_table = pd.read_csv(input_table_popSizeInsens, delim_whitespace=True)
 ## global parameter ( doesn't change) 
 cyc = 200
 sampleInt = 50
-reps = 1
+reps = 5
 toyRun = 0
 if(toyRun==1):
     print("Warning: the toyRun flag is on!")
