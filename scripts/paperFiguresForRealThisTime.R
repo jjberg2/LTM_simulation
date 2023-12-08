61958a30d6a24a3f69008f25663152d15f7ac64e
Jeremy J Berg <jjberg@uchicago.edu>
2023-12-08 13:07:40 -0600

    blah

--- scripts/paperFiguresForRealThisTime.R
+++ scripts/paperFiguresForRealThisTime.R
@@ -16,7 +16,7 @@ par(op1)
 op2 <- par(mar=c(3,3,1,4))
 makePhenLi(xlim=c(-3,4),prev=0.01)
 mtext(side = 1 , text = 'Liability', line = 1.5)
-mtext(side = 2 , text = 'Probability Density on Liability', line = 1.5)
+mtext(side = 2 , text = 'Density on Liability', line = 1.5)
 mtext(side = 3 , text = 'B' , line = 0, at = -3, cex = 1.5)
 par(op2)
 
@@ -27,18 +27,18 @@ this.ymax <- makeGenLi(xlim=c(-3,4),h2=0.6)
 addRiskCurve(h2=h2,prev=0.01,y.max = this.ymax,my.col = this.col)
 axis(side=4,at=seq(0,this.ymax,by=0.2*this.ymax),labels = seq(0,1,by = 0.2),las=1)
 mtext(side = 1 , text = 'Genetic Liability', line = 1.5)
-mtext(side = 2 , text = 'Probability Density on Genetic Liability', line = 1.5)
+mtext(side = 2 , text = 'Density on Genetic Liability', line = 1.5)
 mtext(side = 4 , text = 'Genetic Risk', line = 2.5, col = this.col,las = 0)
 mtext(side = 3 , text = 'C' , line = 0, at = -3, cex = 1.5)
 par(op3)
 
 
 this.col = 'firebrick4'
-op4 <- par(mar=c(3,3,1,4))
+op4 <- par(mar=c(4,3,1,4))
 this.ymax <- makeGenLi(xlim=c(-3,4),h2=0.6)
 addRiskCurve(h2=h2,prev=0.01,y.max = this.ymax,my.col = this.col,fit.surface = TRUE)
 mtext(side = 1 , text = 'Genetic Liability', line = 1.5)
-mtext(side = 2 , text = 'Probability Density on Genetic Liability', line = 1.5)
+mtext(side = 2 , text = 'Density on Genetic Liability', line = 1.5)
 mtext(side = 4 , text = 'Expected fitness', line = 2.5, col = this.col,las = 0)
 axis(side=4,at=seq(0,this.ymax,by=0.2*this.ymax),labels = seq(0,1,by = 0.2),las=1)
 mtext(side = 3 , text = 'D' , line = 0, at = -3, cex = 1.5)
@@ -46,11 +46,11 @@ mtext(side = 3 , text = 'D' , line = 0, at = -3, cex = 1.5)
 
 
 this.col = 'darkorchid4'
-op4 <- par(mar=c(3,3,1,4))
+op4 <- par(mar=c(4,3,1,4))
 prev <- 0.01
 t.pos <- qnorm(1-prev,mean=0,sd=1)
 
-my.risks <- exp(seq ( log(1e-9) , log(1-1e-6) , length.out = 1e7 ))
+my.risks <- exp(seq ( log(1e-9) , log(1-1e-6) , length.out = 1e5 ))
 
 my.g <- qnorm(my.risks , mean = t.pos , sd = sqrt ( 1 - h2 ))
 
@@ -68,7 +68,10 @@ plot(
     NA,
     type = 'l',
     xlim = c ( 0 , 0.2),
-    ylim = c(0,0.5)
+    ylim = c(0,0.5),
+    bty = 'n',
+    yaxt = 'n',
+    xlab = ''
 )
 polygon(
     x = plot.risks ,
@@ -84,7 +87,10 @@ polygon(
     density = 60 ,
     angle = 315
 )
-
+axis(side = 2 , at = seq(0,1,0.2)*0.5,labels = FALSE)
+mtext(side = 1 , text = 'Genetic Risk', line = 2.5,las = 0)
+mtext(side = 2 , text = 'Density on Genetic Risk', line = 1.5)
+mtext(side = 3 , text = 'E' , line = 0, at = 0, cex = 1.5)
 dev.off()
 
 
